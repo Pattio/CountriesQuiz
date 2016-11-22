7ee4bcc42845c5e0f211496233216f18382873b3
Pattio <edvinasbyla@gmail.com>
2016-11-22 15:37:19 +0000

    Add AJAX support to guess

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,10 +1,20 @@
 class GameController < ApplicationController
   include GameHelper
 
+  @answer = nil
+
   def index
     @question = generate_question
     @records = OpenData.where(Description: @question).order("RANDOM()").limit(4)
     @answer = @records.sample
+  end
+
+  def guess
+    guessID = params[:country_id]
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'game#index'
+  match '/' => 'game#guess', via: :post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
